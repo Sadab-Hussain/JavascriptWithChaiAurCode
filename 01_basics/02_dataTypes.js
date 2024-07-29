a9d07e8c04531bb4cafbac64f2343f047388b853
Sadab Hussain <150515281+Sadab-Hussain@users.noreply.github.com>
2024-07-29 12:14:31 +0000

    datTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all js code as newer version
+
+//alert(3+3) // we are using nodejs ,not broweser
+
+console.log( 3 +
+     3 ) // code readability should be high
+    
+console.log("Sadab")
+
+let name = "sadab"
+let age = 18
+let isLoggedIn = false
+let state;
+
+//primitve dataTypes
+// number => 2 to power 53
+// bigint => for big int number
+// string => ""
+// boolean => true/false 
+// null => standlone vlaue
+// undefined =>     
+// symbol => unique
+
+// object 
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
