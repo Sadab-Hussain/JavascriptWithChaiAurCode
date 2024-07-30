4231a395a69be544bb1844990272e3090af44ef2
Sadab Hussain <150515281+Sadab-Hussain@users.noreply.github.com>
2024-07-30 09:44:17 +0000

    StringNumberConversion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,4 +1,4 @@
-let score = "33abc"
+let score = "33abc";
 
 // console.log(typeof score);
 // console.log(typeof (score));
@@ -9,7 +9,7 @@ let valueInNumber = Number(score);
 
 // "33" => 33
 // "33abc" => NaN
-// true => 1; false => 0; 
+// true => 1; false => 0;
 
 // let isLoggedIn = 1
 
@@ -20,8 +20,47 @@ let valueInNumber = Number(score);
 // "" => false
 // "hitesh" => true
 
-let someNumber = 33
+let someNumber = 33;
 
-let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+let stringNumber = String(someNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// ***************** Operations *****************
+
+// let value = 3
+// let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello";
+let str2 = "sadab";
+let str3 = str1 + str2;
+// console.log(str3)
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log((3 + 4) * 5 % 3);
+
+console.log(true);
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3;
+
+num1 = num2 = num3 = 2 + 2;
+
+let gamecounter = 100;
+++gamecounter;
+console.log(gamecounter);
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
