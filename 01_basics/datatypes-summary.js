b9676ad89462a3189af025a32eb31b2f02165abc
Sadab Hussain <150515281+Sadab-Hussain@users.noreply.github.com>
2024-07-30 11:02:57 +0000

    datatypes-summary

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -46,3 +46,26 @@ console.log(typeof scoreValue);
 // Object (native and does not implement [[Call]])	        "object"
 // Object (native or host and does implement [[Call]])	    "function"
 // Object (host and does not implement [[Call]])	        Implementation-defined except may not be "undefined", "boolean", "number", or "string".
+
+// ***************************************************
+
+// Stack (Primitive), Heap (Non-Primitive)
+
+let myYoutubename = "ComputerCodingCenter"
+anothername = "CodeforAll"
+
+console.log(myYoutubename);
+console.log(anothername);
+
+let userOne = {
+    email: "user@google.com",
+    upi: "user@ybl"
+}
+
+let userTwo =  userOne
+
+userTwo.email = "sadab@google.com"
+
+console.log(userOne.email);
+console.log(userOne.email);
+
