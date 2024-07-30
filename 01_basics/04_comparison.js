db42c68f492ca06885ff131cd0dd2d6b0ce5c834
Sadab Hussain <150515281+Sadab-Hussain@users.noreply.github.com>
2024-07-30 09:59:13 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,23 @@
+// basic comparison
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 <= 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// confusion answer
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0); 
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === strict check
+
+console.log("2" === 2);
+
